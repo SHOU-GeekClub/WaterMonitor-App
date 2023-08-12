19231e1d17df916ea8156b403069eaf71cdd1387
Zengfanfan1124 <2458686702@qq.com>
2023-08-12 16:24:32 +0800

    登录接口连接

--- src/screens/login/loginScreen.tsx
+++ src/screens/login/loginScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet} from 'react-native';
+import { View, Text, Alert , StyleSheet} from 'react-native';
 import Icon from '@ant-design/react-native/lib/icon'
 import Button from '@ant-design/react-native/lib/button'
 import InputItem from '@ant-design/react-native/lib/input-item'
@@ -7,6 +7,7 @@ import WhiteSpace from '@ant-design/react-native/lib/white-space'
 import Flex from '@ant-design/react-native/lib/flex'
 import WingBlank from '@ant-design/react-native/lib/wing-blank'
 import Toast from '@ant-design/react-native/lib/toast' 
+import axios from 'axios';
 export default function LoginScreen({navigation}:any){
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -14,18 +15,35 @@ export default function LoginScreen({navigation}:any){
   const handleUsernameChange = (username:any) => {
     setUsername(username);
   };
-
   const handlePasswordChange = (password:any) => {
     setPassword(password);
   };
 
+  //登录函数功能
   const handleLogin = () => {
     // 处理登录逻辑，例如验证用户名和密码等
-
-    // 登录成功后跳转到 mainScreen
-    navigation.navigate('MainScreen');
+    // 发起一个post请求
+    const instance = axios.create({
+      baseURL: 'http://47.102.117.173:5000',
+    });
+    instance.post('/user/login', {
+      username: username,
+      password: password
+    })
+    .then(function (response) {
+      // 如果状态码为200，登录成功后跳转到 mainScresen
+      if(response.status == 200){
+        navigation.navigate('MainScreen');
+      }
+      console.log(response);
+    })
+    .catch(function (error) {
+      showAlert('登录失败', '可能用户名或密码输入错误，请稍后再试。');
+      console.log(error);
+    });
   };
 
+  //注册跳转页面函数
   const handleRegister = () => {
     // 处理注册逻辑，例如验证用户名和密码等
 
@@ -33,6 +51,17 @@ export default function LoginScreen({navigation}:any){
     navigation.navigate('RegisterScreen');
   };
 
+  //提示弹窗
+  const showAlert = (title:any, message:any) => {
+    Alert.alert(
+      title,
+      message,
+      [
+        { text: '确定', onPress: () => console.log('OK Pressed') }
+      ],
+      { cancelable: false }
+    );
+  };
   return (
     <View>
       <Text style={styles.text}>登录页面</Text>
